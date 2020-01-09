464189211178c23f04356093e21340a1ad0e8802
李梦龙 <649076408@qq.com>
2020-01-09 17:12:43 +0800

    feat($function): add flat

---
+++ .eslintrc.js
@@ -0,0 +1,67 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'airbnb'
+  ],
+  plugins: [],
+  rules: {
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        ts: 'never',
+        js: 'never',
+      },
+    ],
+    'class-methods-use-this': [
+      'error',
+      {
+        exceptMethods: [
+          'render',
+          'change',
+          'changeHandle',
+          'beforeHandle',
+          'setTitle',
+          'bindKeyUp',
+          'setScroll',
+          'returnModel',
+          'popoverClick',
+        ],
+      },
+    ],
+    'no-unused-vars': [
+      'error',
+      { varsIgnorePattern: '^V|Moment|Entity$|ENUM$' },
+    ],
+    'no-underscore-dangle': ['error', { allow: ['_componentTag'] }],
+    'spaced-comment': 'off',
+    'no-dupe-keys': 'off',
+    'no-console': 'always',
+    'no-extend-native': 'off',
+    'no-plusplus': 'off',
+    'max-len': [
+      'error',
+      {
+        code: 100,
+        ignoreTrailingComments: true,
+        ignoreComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        optionalDependencies: ['test/unit/index.js'],
+      },
+    ],
+    'no-param-reassign': ['error', { props: false }],
+    'no-console': 'error',
+    'no-debugger': 'error'
+  }
+};
